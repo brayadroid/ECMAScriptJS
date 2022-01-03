dd08609974de814660c0424562957cc21d222b59
brayan moya fernandez <brayadroid@gmail.com>
2022-01-03 13:39:20 -0300

    Que implemento ES7

---
+++ ES7/index.js
@@ -0,0 +1,16 @@
+//ES7
+
+let numbers = [1,2,3,4,5];
+
+if(numbers.includes(4)){
+    console.log('Si se encuentra el valor 7');
+}else{
+    console.log('No se encuentra');
+}
+
+//Elevar a la potencia
+let base = 4;
+let exponente = 2;
+let resultado = base ** exponente;
+
+console.log(resultado);

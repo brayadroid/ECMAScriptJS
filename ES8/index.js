3c4e029e0c4e52e7581d6ca7fe1ea47a315ef04f
brayan moya fernandez <brayadroid@gmail.com>
2022-01-03 16:55:19 -0300

    ES8 asincronismo y promesas

--- ES8/index.js
+++ ES8/index.js
@@ -26,3 +26,29 @@ console.log(valores.length);
 const string = 'hello';
 console.log(string.padStart(7,"hi"));
 console.log(string.padEnd(12,"-----------"));
+
+const helloWorld = () => {
+    return new Promise((resolve,reject) => {
+        (true)
+            ?  setTimeout(()=>resolve('Hello wordl'),3000)
+            : reject(new Error('Test error'))
+    })
+}
+
+const helloAsync = async() => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const otraFuncion = async() => {
+    try{
+        const hello = await helloWorld();
+        console.log(hello)
+    }catch(error){
+        console.log(error);
+    }
+}
+
+otraFuncion();
